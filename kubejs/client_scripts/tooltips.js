89653765f807f3db183301f5b36adb946029421d
Zizazr <filovtv999@gmail.com>
2026-06-20 18:51:34 +0300

    fix crush

--- kubejs/client_scripts/tooltips.js
+++
@@ -1,7 +0,0 @@
-ItemEvents.tooltip(event => {
-    event.addAdvanced("computercraft:disk", (item, advanced, text) => {
-        text.add(1, Text.of('Floppa disk').yellow());
-        text.add(2, Text.of('5 МБ').green());
-
-    })
-})
