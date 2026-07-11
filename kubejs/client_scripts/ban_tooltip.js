9a1e3d36e32da5780846abfdbcad8607e39a1764
Zizazar <filovtv999@gmail.com>
2026-07-11 22:46:09 +0300

    rem tooltips

--- kubejs/client_scripts/ban_tooltip.js
+++
@@ -1,8 +0,0 @@
-ItemEvents.tooltip(event => {
-    event.addAll(item => {
-        if (item.hasTag('kubejs:banned')) {
-            return [Component.red('ЗАБАНЕН').bold(true)]
-        }
-        return []
-    })
-})
